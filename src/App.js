4b32945b1bcc006e94da90df16cb8c9a086135b0
Agner <65578473+AgnerLoss@users.noreply.github.com>
2025-11-15 08:17:41 -0300

    Atualizar o App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Esse é meu Primeiro App.
+          Projeto Prático para CodePipeline.
         </p>
         <a
           className="App-link"
